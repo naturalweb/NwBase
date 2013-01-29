0a37a6feeee226af9da642cb480ba5268f1d4054
Renato Moura <renato@naturalweb.com.br>
2013-01-29 14:31:44 -0200

    Padronizado os padrões de codigos das classes de Paginator, definido comentarios

--- src/NwBase/Paginator/Adapter/DbTableSelect.php
+++ src/NwBase/Paginator/Adapter/DbTableSelect.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * Natural Web Ltda. (http://www.naturalweb.com.br)
+ *
+ * @copyright  Copyright (c) Natural Web Ltda. (http://www.naturalweb.com.br)
+ * @license    BSD-3-Clause
+ * @package    NwBase\Paginator
+ * @subpackage Adapter
+ */
 namespace NwBase\Paginator\Adapter;
 
 use Zend\Paginator\Adapter\AdapterInterface;
@@ -7,33 +14,47 @@ use Zend\Paginator\Adapter\DbSelect;
 use Zend\Db\Sql\Select;
 use NwBase\Model\InterfaceModel;
 
+/**
+ * Adapter do Paginator para trabalhar com objeto InterfaceModel
+ *
+ * @category   NwBase
+ * @package    NwBase\Paginator
+ * @subpackage Adapter
+ * @author     Renato Moura <renato@naturalweb.com.br>
+ */
 class DbTableSelect implements AdapterInterface
 {
-    /**
-     * 
+    /** 
      * @var DbSelect
      */
-    private $dbSelect;
+    private $_dbSelect;
     
+    /**
+     * Construtor
+     * 
+     * @param Select         $select Object Select
+     * @param InterfaceModel $model  Object Model
+     */
     public function __construct(Select $select, InterfaceModel $model)
     {
         $tableGateway       = $model->getTableGateway();
         $dbAdapter          = $tableGateway->getAdapter();
         $resultSetPrototype = $tableGateway->getResultSetPrototype();
         
-        $this->dbSelect = new DbSelect($select, $dbAdapter, $resultSetPrototype);
+        $this->_dbSelect = new DbSelect($select, $dbAdapter, $resultSetPrototype);
     }
     
     /**
      * Returns an array of items for a page.
      *
-     * @param  integer $offset           Page offset
-     * @param  integer $itemCountPerPage Number of items per page
+     * @param int $offset           Page offset
+     * @param int $itemCountPerPage Number of items per page
+     * 
      * @return Zend\Db\ResultSet\ResultSet
      */
     public function getItems($offset, $itemCountPerPage)
     {
-        return $this->dbSelect->getItems($offset, $itemCountPerPage);
+        return $this->_dbSelect->getItems($offset, $itemCountPerPage);
     }
     
     /**
@@ -43,6 +64,6 @@ class DbTableSelect implements AdapterInterface
      */
     public function count()
     {
-        return $this->dbSelect->count();
+        return $this->_dbSelect->count();
     }
 }
