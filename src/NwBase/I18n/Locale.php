ee5d52b0b2c57e4174fb0b19e6b05db314375d44
Renato Moura <renato@naturalweb.com.br>
2013-01-31 11:39:50 -0200

    Adicionado Class Locale, para funções de listagem de ufs

---
+++ src/NwBase/I18n/Locale.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Natural Web Ltda. (http://www.naturalweb.com.br)
+ *
+ * @copyright Copyright (c) Natural Web Ltda. (http://www.naturalweb.com.br)
+ * @license   BSD-3-Clause
+ * @package   NwBase\I18n
+ */
+namespace NwBase\I18n;
+
+/**
+ * Classe com conjunto de funções sobre a localização do projeto
+ *
+ * @category NwBase
+ * @package  NwBase\I18n
+ * @author   Renato Moura <renato@naturalweb.com.br>
+ */
+class Locale extends \Locale
+{
+    /**
+     * Lista de UFs
+     * @var array
+     */
+    protected static $_listUF = array(
+        'BR' => array(
+            'AC' => 'Acre',
+            'AL' => 'Alagoas',
+            'AP' => 'Amapá',
+            'AM' => 'Amazonas',
+            'BA' => 'Bahia',
+            'CE' => 'Ceara',
+            'DF' => 'Distrito Federal',
+            'ES' => 'Espírito Santo',
+            'GO' => 'Goias',
+            'MA' => 'Maranhão',
+            'MT' => 'Mato Grosso',
+            'MS' => 'Mato Grosso do Sul',
+            'MG' => 'Minas Gerais',
+            'PA' => 'Pará',
+            'PB' => 'Paraiba',
+            'PR' => 'Paraná',
+            'PE' => 'Pernambuco',
+            'PI' => 'Piauí',
+            'RJ' => 'Rio de Janeiro',
+            'RN' => 'Rio Grande do Norte',
+            'RS' => 'Rio Grande do Sul',
+            'RO' => 'Rondônia',
+            'RR' => 'Roraima',
+            'SC' => 'Santa Catarina',
+            'SP' => 'São Paulo',
+            'SE' => 'Sergipe',
+            'TO' => 'Tocantins',
+        ),
+    );
+    
+    /**
+     * Retorna a lista de UF (Estados) do pais,
+     * com a sigla de chave e nome de valor
+     *
+     * @return array
+     */
+    public static function nomesUF($locale = null)
+    {
+        $list = array();
+        if (!$locale) {
+            $locale = self::getDefault();
+        }
+        
+        $region = self::getRegion($locale);
+        if (isset(self::$_listUF[$region])) {
+            $list = self::$_listUF[$region];
+        }
+        
+        return $list;
+    }
+    
+    /**
+     * Retorna a lista de UF (Estados) do pais,
+     * com a sigla de chave e valor
+     *
+     * @return array
+     */
+    public static function siglasUF($locale = null)
+    {
+        $list = self::nomesUF($locale);
+        array_walk(
+            $list,
+            function(&$item, $key){
+                $item = $key;
+            }
+        );
+        
+        return $list;
+    }
+}
---
+++ tests/NwBaseTest/I18n/LocaleTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace NwBaseTest\I18n;
+
+use NwBase\I18n\Locale;
+
+class LocaleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNomeUF()
+    {
+        $expected = array(
+            'AC' => 'Acre',
+            'AL' => 'Alagoas',
+            'AP' => 'Amapá',
+            'AM' => 'Amazonas',
+            'BA' => 'Bahia',
+            'CE' => 'Ceara',
+            'DF' => 'Distrito Federal',
+            'ES' => 'Espírito Santo',
+            'GO' => 'Goias',
+            'MA' => 'Maranhão',
+            'MT' => 'Mato Grosso',
+            'MS' => 'Mato Grosso do Sul',
+            'MG' => 'Minas Gerais',
+            'PA' => 'Pará',
+            'PB' => 'Paraiba',
+            'PR' => 'Paraná',
+            'PE' => 'Pernambuco',
+            'PI' => 'Piauí',
+            'RJ' => 'Rio de Janeiro',
+            'RN' => 'Rio Grande do Norte',
+            'RS' => 'Rio Grande do Sul',
+            'RO' => 'Rondônia',
+            'RR' => 'Roraima',
+            'SC' => 'Santa Catarina',
+            'SP' => 'São Paulo',
+            'SE' => 'Sergipe',
+            'TO' => 'Tocantins',
+        );
+        
+        $actual = Locale::nomesUF('pt_BR');
+        $this->assertEquals($expected, $actual);
+    }
+    
+    public function testSiglasUF()
+    {
+        $expected = array(
+            'AC' => 'AC', 'AL' => 'AL',
+            'AP' => 'AP', 'AM' => 'AM',
+            'BA' => 'BA', 'CE' => 'CE',
+            'DF' => 'DF', 'ES' => 'ES',
+            'GO' => 'GO', 'MA' => 'MA',
+            'MT' => 'MT', 'MS' => 'MS',
+            'MG' => 'MG', 'PA' => 'PA',
+            'PB' => 'PB', 'PR' => 'PR',
+            'PE' => 'PE', 'PI' => 'PI',
+            'RJ' => 'RJ', 'RN' => 'RN',
+            'RS' => 'RS', 'RO' => 'RO',
+            'RR' => 'RR', 'SC' => 'SC',
+            'SP' => 'SP', 'SE' => 'SE',
+            'TO' => 'TO');
+        
+        $actual = Locale::siglasUF('pt_BR');
+        $this->assertEquals($expected, $actual);
+    }
+}
