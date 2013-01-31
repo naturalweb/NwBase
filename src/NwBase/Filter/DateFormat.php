ef71ce766fb570e89a14ecedc611b1dda65edf05
Renato Moura <renato@naturalweb.com.br>
2013-01-31 11:40:53 -0200

    Criado função de filter de data, para retorar o objeto de datetime

---
+++ src/NwBase/Filter/DateFormat.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Natural Web Ltda. (http://www.naturalweb.com.br)
+ *
+ * @copyright Copyright (c) Natural Web Ltda. (http://www.naturalweb.com.br)
+ * @license   BSD-3-Clause
+ * @package   NwBase\Filter
+ */
+namespace NwBase\Filter;
+
+use Zend\Filter\AbstractFilter;
+use Locale;
+use DateTime;
+
+/**
+ * Filtrar, removendo os caracteres adicionais, padrão espaço removendo no meio e no inicio e fim
+ * 
+ * @category NwBase
+ * @package  NwBase\Filter
+ * @author   Renato Moura <renato@naturalweb.com.br>
+ */
+class DateFormat extends AbstractFilter
+{
+    /**
+     * @var array
+     */
+    protected $options = array(
+        'format' => null,
+    );
+    
+    /**
+     * Sets filter options
+     *
+     * @param  string|array|Traversable $formatOrOptions
+     */
+    public function __construct($formatOrOptions = null)
+    {
+        if ($formatOrOptions !== null) {
+            if (!is_array($formatOrOptions)
+                    && !$formatOrOptions  instanceof Traversable)
+            {
+                $this->setFormat($formatOrOptions);
+            } else {
+                $this->setOptions($formatOrOptions);
+            }
+        }
+    }
+    
+    /**
+     * Sets the format option
+     *
+     * @param  string $format
+     * @return StringTrim Provides a fluent interface
+     */
+    public function setFormat($format)
+    {
+        if (empty($format)) {
+            $format = null;
+        }
+        $this->options['format'] = $format;
+        return $this;
+    }
+    
+    /**
+     * Returns the format option
+     *
+     * @return string|null
+     */
+    public function getFormat()
+    {
+        return $this->options['format'];
+    }
+    
+    /**
+     * Filtra a Data criando um objeto DateTime
+     */
+    public function filter($value)
+    {
+        if ($value instanceof DateTime) {
+            return $value;
+        }
+        
+        $format = $this->getFormat();
+        if (!$format) {
+            return null;
+        }
+        
+        $date = DateTime::createFromFormat($format, $value);
+        if (!$date) {
+            $date = null;
+        }
+        
+        return $date;
+    }
+}
---
+++ tests/NwBaseTest/Filter/DateFormatTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace NwBaseTest\Filter;
+
+use NwBase\Filter\DateFormat;
+use DateTime;
+
+class DateFormatTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDateFormatValid()
+    {
+        $format = 'd/m/Y';
+        $time = '31/12/2012';
+        $expectedDate = DateTime::createFromFormat($format, $time);
+        
+        $filterDate = new DateFormat($format);
+        $actualDate = $filterDate->filter($time);
+        
+        $this->assertInstanceOf('\DateTime', $actualDate);
+        $this->assertEquals($expectedDate, $actualDate);
+    }
+    
+    public function testDateFormatInvalid()
+    {
+        $format = 'd/m/Y';
+        $time = '2012-12-31';
+        
+        $filterDate = new DateFormat($format);
+        $actualDate = $filterDate->filter($time);
+        
+        $this->assertNull($actualDate);
+    }
+    
+    public function testDateWithNotFormat()
+    {
+        $time = '31/12/2012';
+    
+        $filterDate = new DateFormat();
+        $actualDate = $filterDate->filter($time);
+        
+        $this->assertNull($actualDate);
+    }
+    
+    public function testDateFormatWithObject()
+    {
+        $time = new DateTime('2013-12-31');
+    
+        $filterDate = new DateFormat();
+        $actualDate = $filterDate->filter($time);
+    
+        $this->assertInstanceOf('\DateTime', $actualDate);
+        $this->assertEquals($time, $actualDate);
+    }
+}
