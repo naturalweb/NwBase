9b74255810a6f7ec99bdf4dbca676a53ac6f89b6
Renato Moura <renato@naturalweb.com.br>
2013-02-18 11:58:04 -0300

    Adicioando AbstractForm, que implementa o ServiceLocatorWareInterface, para utilização dos forms nos projetos

---
+++ src/NwBase/Form/AbstractForm.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Natural Web Ltda. (http://www.naturalweb.com.br)
+ *
+ * @copyright Copyright (c) Natural Web Ltda. (http://www.naturalweb.com.br)
+ * @license   BSD-3-Clause
+ * @package   NwBase\Form
+ */
+namespace NwBase\Form;
+
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Classe abstrata para criação de Form
+ * 
+ * @category NwBase
+ * @package  NwBase\Form
+ * @author   Renato Moura <renato@naturalweb.com.br>
+ * @abstract
+ */
+abstract class AbstractForm implements ServiceLocatorAwareInterface
+{
+    /**
+     * 
+     * @var ServiceLocatorInterface
+     */
+    protected $serviceLocator = null;
+    
+    /**
+     * Set serviceManager instance
+     *
+     * @param ServiceLocatorInterface $serviceLocator Objeto de Service
+     * 
+     * @return void
+     */
+    public function setServiceLocator(ServiceLocatorInterface $serviceLocator)
+    {
+        $this->serviceLocator = $serviceLocator;
+        return $this;
+    }
+    
+    /**
+     * Retrieve serviceManager instance
+     *
+     * @return ServiceLocatorInterface
+     */
+    public function getServiceLocator()
+    {
+        return $this->serviceLocator;
+    }
+}
---
+++ tests/NwBaseTest/Form/AbstractFormTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace NwBaseTest\Form;
+
+use NwBase\Form\AbstractForm;
+
+class AbstractFormTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAbstractForm()
+    {
+        $mockAbstractForm = $this->getMockForAbstractClass('NwBase\Form\AbstractForm');
+        $this->assertInstanceOf('Zend\ServiceManager\ServiceLocatorAwareInterface', $mockAbstractForm);
+    }
+    
+    public function testAbstractFormSetService()
+    {
+        $mockAbstractForm = $this->getMockForAbstractClass('NwBase\Form\AbstractForm');
+        $mockService = $this->getMock('Zend\ServiceManager\ServiceLocatorInterface');
+        $return = $mockAbstractForm->setServiceLocator($mockService);
+        
+        $this->assertAttributeEquals($mockService, 'serviceLocator', $mockAbstractForm);
+        $this->assertEquals($mockAbstractForm, $return);
+    }
+    
+    public function testAbstractFormGetService()
+    {
+        $mockAbstractForm = $this->getMockForAbstractClass('NwBase\Form\AbstractForm');
+        $mockService = $this->getMock('Zend\ServiceManager\ServiceLocatorInterface');
+        $mockAbstractForm->setServiceLocator($mockService);
+    
+        $this->assertEquals($mockService, $mockAbstractForm->getServiceLocator());
+    }
+}
