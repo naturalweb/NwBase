df67d70fccac2e178a150368b35121b2ecc0c8af
Renato Moura <renato@naturalweb.com.br>
2013-03-19 12:02:43 -0300

    Criado element do formulario Referer para utilização no form para salver a url de referencia

---
+++ src/NwBase/Form/Element/Referer.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Natural Web Ltda. (http://www.naturalweb.com.br)
+*
+* @copyright Copyright (c) Natural Web Ltda. (http://www.naturalweb.com.br)
+* @license   BSD-3-Clause
+* @package   NwBase\Form\Element
+*/
+
+namespace NwBase\Form\Element;
+
+use Zend\Form\Element;
+
+/**
+ * Element do Formulario, para salvar a url de referencia,
+ * para o futuro redirecionamento
+ *
+ * @category NwBase
+ * @package  NwBase\Form\Element
+ * @author   Renato Moura <renato@naturalweb.com.br>
+ */
+class Referer extends Element
+{
+    /**
+     * Seed attributes
+     *
+     * @var array
+     */
+    protected $attributes = array(
+        'type' => 'hidden',
+    );
+    
+    /**
+     * Retrieve the element value, salvando o referer quanto não tiver value
+     *
+     * @return mixed
+     */
+    public function getValue()
+    {
+        if (!is_null($this->value)) {
+            return $this->value;
+        }
+        
+        if (isset($_SERVER['HTTP_REFERER'])) {
+            $value = $_SERVER['HTTP_REFERER'];
+        } else {
+            $value = '';
+        }
+        
+        $this->setValue($value);
+        return $value;
+    }
+}
---
+++ tests/NwBaseTest/Form/Element/RefererTest.php
@@ -0,0 +1,45 @@
+<?php
+namespace NwBaseTest\Form\Element;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use NwBase\Form\Element\Referer as RefererElement;
+
+class RefererTest extends TestCase
+{
+    protected function tearDown()
+    {
+        if (isset($_SERVER['HTTP_REFERER'])) {
+            unset($_SERVER['HTTP_REFERER']);
+        }
+    }
+ 
+    public function testElementAttributesAndValueDefault()
+    {
+        $element = new RefererElement();
+        
+        $this->assertInstanceOf('Zend\Form\Element', $element);
+        $this->assertSame('', $element->getValue());
+    }
+    
+    public function testValueWithReferer()
+    {
+        $referer = "http://localhost/teste";
+        $_SERVER['HTTP_REFERER'] = $referer;
+        
+        $element = new RefererElement();
+        
+        $this->assertEquals($referer, $element->getValue());
+    }
+    
+    public function testValueUpdateWithReferer()
+    {
+        $referer = "http://localhost/teste";
+        $_SERVER['HTTP_REFERER'] = $referer;
+        
+        $element = new RefererElement();
+        $value = "http://host.atual";
+        $element->setValue($value);
+        
+        $this->assertEquals($value, $element->getValue());
+    }
+}
