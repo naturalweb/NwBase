6b5622d857cb774fed181802ab654c626a6ec824
Renato Moura <renato@naturalweb.com.br>
2013-06-26 15:17:11 -0300

    Criado o adapter ProgressBar, alterando o JsPush adicionando uma opção de inserir parametros para o metodo finish

---
+++ src/NwBase/ProgressBar/Adapter/JsPush.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Natural Web Ltda. (http://www.naturalweb.com.br)
+ *
+ * @copyright 2013 - Copyright (c) Natural Web Ltda. (http://www.naturalweb.com.br)
+ * @license   BSD-3-Clause http://opensource.org/licenses/BSD-3-Clause
+ */
+namespace NwBase\ProgressBar\Adapter;
+
+use Zend\ProgressBar\Adapter\JsPush as ZendJsPush;
+use Zend\Json\Json;
+
+/**
+ * Adapter do ProgressBar, para definir parametro do metodo finish
+ *
+ * @category   NwBase
+ * @package    NwBase\ProgressBar\Adapter
+ * @author     Renato Moura <renato@naturalweb.com.br>
+ */
+class JsPush extends ZendJsPush
+{
+    /**
+     * Parametros para o metodo finish
+     *
+     * @var string
+     */
+    protected $finishParameters;
+    
+    /**
+     * Set the finish method name
+     *
+     * @param  string $methodName
+     * @return \Zend\ProgressBar\Adapter\JsPush
+     */
+    public function setFinishParameters($parameters)
+    {
+        if (is_array($parameters)) {
+            $parameters = Json::encode($parameters);
+            
+        } elseif (!is_scalar($parameters)) {
+            $parameters = null;
+        }
+        
+        $this->finishParameters = $parameters;
+        
+        return $this;
+    }
+    
+    /**
+     * Defined by Zend\ProgressBar\Adapter\AbstractAdapter
+     * Com os parametros de finish
+     *
+     * @return void
+     */
+    public function finish()
+    {
+        if ($this->finishMethodName === null) {
+            return;
+        }
+        
+        $data = '<script type="text/javascript">'
+        . 'parent.' . $this->finishMethodName . '(' . $this->finishParameters . ');'
+        . '</script>';
+        
+        $this->_outputData($data);
+    }
+}
---
+++ tests/NwBaseTest/ProgressBar/Adapter/JsPushTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace NwBaseTest\ProgressBar\Adapter;
+
+use \PHPUnit_Framework_TestCase as TestCase;
+
+class JsPushTest extends TestCase
+{
+    public function testSetFinishParametersWithArray()
+    {
+        $parameters = array('query' => 'foobar');
+        
+        $adapter = new JsPushStub();
+        
+        $return = $adapter->setFinishParameters($parameters);
+        
+        $this->assertAttributeEquals(\Zend\Json\Json::encode($parameters), 'finishParameters', $adapter);
+        $this->assertEquals($adapter, $return, "Deveria retornar a propria instancia");
+    }
+    
+    public function testSetFinishParametersWithString()
+    {
+        $parameters = 'foobar';
+    
+        $adapter = new JsPushStub();
+    
+        $return = $adapter->setFinishParameters($parameters);
+    
+        $this->assertAttributeEquals($parameters, 'finishParameters', $adapter);
+        $this->assertEquals($adapter, $return, "Deveria retornar a propria instancia");
+    }
+    
+    public function testSetFinishParametersNotWithScalar()
+    {
+        $parameters = new \stdClass();
+    
+        $adapter = new JsPushStub();
+    
+        $return = $adapter->setFinishParameters($parameters);
+    
+        $this->assertAttributeEquals(null, 'finishParameters', $adapter);
+        $this->assertEquals($adapter, $return, "Deveria retornar a propria instancia");
+    }
+    
+    public function testMethodFinishWithParameters()
+    {
+        $parameters = array('query' => 'foobar');
+        
+        $adapter = new JsPushStub(array('finishMethodName' => 'Zend\ProgressBar\ProgressBar\Finish', 'finishParameters' => $parameters));
+        $adapter->notify(0, 2, 0.5, 1, 1, 'status');
+        $adapter->finish();
+        $output = $adapter->getLastOutput();
+        
+        $matches = preg_match('#<script type="text/javascript">parent.'. preg_quote('Zend\ProgressBar\ProgressBar\Finish') . '\((.*?)\);</script>#', $output, $result);
+        
+        $this->assertEquals(1, $matches);
+        
+        $data = json_decode($result[1], true);
+        
+        $this->assertEquals('foobar', $data['query']);
+    }
+}
+
+class JsPushStub extends \NwBase\ProgressBar\Adapter\JsPush
+{
+    protected $_lastOutput = null;
+
+    public function getLastOutput()
+    {
+        return $this->_lastOutput;
+    }
+
+    protected function _outputData($data)
+    {
+        $this->_lastOutput = $data;
+    }
+}
