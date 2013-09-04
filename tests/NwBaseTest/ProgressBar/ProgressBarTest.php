eca809ed90a6a47e6f7ddbacb711ea7b916165f7
Edson Junior <edson.junior@becinformacao.com.br>
2013-09-04 11:12:14 -0300

    Corrigido teste do ProgressBar.

--- tests/NwBaseTest/ProgressBar/ProgressBarTest.php
+++ tests/NwBaseTest/ProgressBar/ProgressBarTest.php
@@ -7,9 +7,10 @@ class ProgressBarTest extends \PHPUnit_Framework_TestCase
 {
     public function testEncerra()
     {
-        $adapter = $this->getMock('NwBase\ProgressBar\Adapter\JsPush', array('encerra'));
+        $adapter = $this->getMock('NwBase\ProgressBar\Adapter\JsPush');
         $adapter->expects($this->once())
-                ->method('encerra');
+                ->method('encerra')
+        		->will($this->returnValue(true));
         
         $progressBar = new ProgressBar($adapter);
         $progressBar->encerra();
