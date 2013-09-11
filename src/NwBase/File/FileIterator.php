739e426b0f89ea453a64f4c390aca74ee368950b
Renato Moura <renato@naturalweb.com.br>
2013-09-11 17:54:12 -0300

    Criado a class FileIterator para percorrer arquivos

---
+++ src/NwBase/File/FileIterator.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ * Natural Web Ltda. (http://www.naturalweb.com.br)
+ *
+ * @copyright 2013 - Copyright (c) Natural Web Ltda. (http://www.naturalweb.com.br)
+ * @license   BSD-3-Clause http://opensource.org/licenses/BSD-3-Clause
+ */
+namespace NwBase\File;
+
+/**
+ * Efetua a leitura de um arquivo, iplementa o iterator para percorrer as linhas do arquivo  
+ *
+ * @category NwBase
+ * @package  NwBase\File
+ * @author   Renato Moura <renato@naturalweb.com.br>
+ */
+class FileIterator implements \Iterator, \Countable
+{
+    /** 
+     * @var resource
+     */
+    protected $fileHandle;
+    
+    /** 
+     * @var string
+     */
+    protected $lineCurrent;
+    
+    /** 
+     * @var int
+     */
+    protected $key;
+    
+    /** 
+     * @var int
+     */
+    protected $count;
+    
+    /**
+     * Abre o arquivo para leitura
+     * 
+     * @param string $fileName Path
+     * 
+     * @throws \RuntimeException
+     */
+    public function __construct($fileName)
+    {
+        if (!$this->fileHandle = fopen($fileName, 'r')) {
+            throw new \RuntimeException('Couldn\'t open file "' . $fileName . '"');
+        }
+    }
+    
+    /**
+     * Contagem de linhas
+     * 
+     * @return int
+     */
+    public function count()
+    {
+        if (is_null($this->count)) {
+            $this->count = 0;
+            
+            $tell = ftell($this->fileHandle); 
+            fseek($this->fileHandle, 0);
+            
+            while (!feof($this->fileHandle) && false !== fgets($this->fileHandle)) {
+                $this->count += 1;
+            }
+            
+            fseek($this->fileHandle, $tell);
+            unset($tell);
+        }
+        
+        return $this->count;
+    }
+    
+    /**
+     * Inicia a leitura do arquivo do inicio
+     * 
+     * @return void
+     */
+    public function rewind()
+    {
+        fseek($this->fileHandle, 0);
+        $this->lineCurrent = $this->getLine();
+        $this->key = 0;
+    }
+    
+    /**
+     * Retorna o indice atual
+     * 
+     * @return int
+     */
+    public function key()
+    {
+        return $this->key;
+    }
+    
+    /**
+     * Valida se havera proxima linha
+     * 
+     * @return bool
+     */
+    public function valid()
+    {
+        return !feof($this->fileHandle) && false !== $this->lineCurrent;
+    }
+    
+    /**
+     * Retorna a linha atual
+     * 
+     * @return string
+     */
+    public function current()
+    {
+        return $this->lineCurrent;
+    }
+    
+    /**
+     * Busca a proxima linha
+     * 
+     * @return void
+     */
+    public function next()
+    {
+        if ($this->valid()) {
+            $this->lineCurrent = $this->getLine();
+            $this->key++;
+        }
+    }
+    
+    /**
+     * Le alinha no arquivo, formata o encodig caso seja necessario
+     * 
+     * @return string
+     */
+    protected function getLine()
+    {
+        $encoding = 'UTF-8';
+        
+        $line = fgets($this->fileHandle);
+        
+        if (mb_detect_encoding($line, $encoding, true) == $encoding) {
+            $line = utf8_encode($line);
+        }
+        
+        return trim($line);
+    }
+    
+    /**
+     * Ao destruir fecha o arquivo
+     * 
+     * @return void
+     */
+    public function __destruct()
+    {
+        if (is_resource($this->fileHandle)) {
+            fclose($this->fileHandle);
+        }
+    }
+}
---
+++ tests/NwBaseTest/File/FileIteratorTest.php
@@ -0,0 +1,67 @@
+<?php
+namespace NwBaseTest\File;
+
+use NwBase\File\FileIterator;
+
+class FileIteratorTest extends \PHPUnit_Framework_TestCase
+{
+    protected $filename;
+    
+    protected $totLine = 4;
+    
+    public function setUp()
+    {
+        $content = '';
+        for($x=0;$x<$this->totLine;$x++) {
+            $content .= "Line " . $x . PHP_EOL;
+        }
+        
+        $this->filename = tempnam('', '');
+        file_put_contents($this->filename, trim($content));
+    }
+    
+    public function tearDown()
+    {
+        unlink($this->filename);
+    }
+    
+    protected function assertPreConditions()
+    {
+        $this->assertTrue(
+            class_exists($class = 'NwBase\File\FileIterator'),
+            "Classe {$class} não existe"
+        );
+    }
+    
+    public function testConstrunctAndIntances()
+    {
+        $iterator = new FileIterator($this->filename);
+        
+        $this->assertInstanceOf('\Iterator', $iterator);
+        $this->assertInstanceOf('\Countable', $iterator);
+        $this->assertAttributeEquals(null, 'key', $iterator);
+        $this->assertAttributeEquals(null, 'lineCurrent', $iterator);
+        $this->assertAttributeEquals(null, 'count', $iterator);
+    }
+    
+    public function testCountable()
+    {
+        $iterator = new FileIterator($this->filename);
+        
+        $this->assertEquals($this->totLine, $iterator->count());
+        $this->assertAttributeEquals($this->totLine, 'count', $iterator);
+    }
+    
+    public function testLoopInInstance()
+    {
+        $iterator = new FileIterator($this->filename);
+        
+        $i=0;
+        foreach ($iterator as $key => $line)
+        {
+            $this->assertEquals($i, $key);
+            $this->assertEquals('Line ' . $i, $line);
+            $i++;
+        }
+    }
+}
